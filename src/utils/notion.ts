37aea6d4f13bfff69fa12215a3c92e3eff8bba77
Diego Hernández Herrera <diegohh0411@gmail.com>
2025-01-20 21:21:30 -0600

    Ticket route implemented

---
+++ src/utils/notion.ts
@@ -0,0 +1,7 @@
+import { Client } from "@notionhq/client";
+
+export const notion = new Client({
+    auth: "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"
+})
+
+export const database_registration_id = "18162de3ddab8004b6d8d3e7fe6416ad";
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,28 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['class'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-    	extend: {
-    		fontFamily: {
-    			sans: [
-    				'Acumin',
-    				'sans-serif'
-    			],
-    			serif: [
-    				'Moret',
-    				'serif'
-    			]
-    		},
-    		colors: {
-    			primary: 'rgb(164, 189, 255)'
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		}
-    	}
-    },
+		extend: {
+			fontFamily: {
+				sans: [
+					'Acumin',
+					'sans-serif'
+				],
+				serif: [
+					'Moret',
+					'serif'
+				],
+				mono: [
+					'"Roboto mono"',
+					'mono'
+				]
+			},
+			colors: {
+				primary: 'rgb(164, 189, 255)'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
 	plugins: [require("tailwindcss-animate")],
 }
